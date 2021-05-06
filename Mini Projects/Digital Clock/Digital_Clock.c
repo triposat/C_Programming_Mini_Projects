635f81be06bd09134b7d27335b66d2b971f96c61
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-06 11:03:34 +0530

    Update Digital_Clock.c

--- Mini Projects/Digital Clock/Digital_Clock.c
+++ Mini Projects/Digital Clock/Digital_Clock.c
@@ -1,43 +1,45 @@
+// Note: Try to run this Code on DEV-C++
+
 #include <stdio.h>
-#include <time.h> 
+#include <time.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include<windows.h>
- int main()
+#include <windows.h>
+int main()
 {
-	time_t t = time(NULL);
-  struct tm tm = *localtime(&t);
-	system("color 1f");
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    system("color 1f");
     int hour, minute, second;
-    hour=minute=second=0;
-    hour=tm.tm_hour;
-    minute=tm.tm_min;
-    second=tm.tm_sec;
- while(1)
+    hour = minute = second = 0;
+    hour = tm.tm_hour;
+    minute = tm.tm_min;
+    second = tm.tm_sec;
+    while (1)
     {
-    	system("cls");
-    	 printf("\n\n\n\n\n\t\t\t\tCurrent Time is :");
-        printf("\n\n\n\t\t\t\t %02d : %02d : %02d ",hour,minute,second);
+        system("cls");
+        printf("\n\n\n\n\n\t\t\t\tCurrent Time is :");
+        printf("\n\n\n\t\t\t\t %02d : %02d : %02d ", hour, minute, second);
         second++;
-        if(second==60)
-		{
-            minute=minute+1;
-            second=0;
+        if (second == 60)
+        {
+            minute = minute + 1;
+            second = 0;
         }
-        if(minute==60)
-		{
-            hour=hour+1;
-            minute=0;
+        if (minute == 60)
+        {
+            hour = hour + 1;
+            minute = 0;
         }
-        if(hour==24)
-		{
-            hour=0;
-            minute=0;
-            second=0;
+        if (hour == 24)
+        {
+            hour = 0;
+            minute = 0;
+            second = 0;
         }
-         
+
         Sleep(1000);
     }
- 
+
     return 0;
 }
