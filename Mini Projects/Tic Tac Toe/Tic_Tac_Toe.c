2604707e9217e73510cb8b6ec8863c9ae8855419
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-06 11:13:33 +0530

    Update Tic_Tac_Toe.c

--- Mini Projects/Tic Tac Toe/Tic_Tac_Toe.c
+++ Mini Projects/Tic Tac Toe/Tic_Tac_Toe.c
@@ -1,8 +1,10 @@
+// Note: Try to run this Code on DEV-C++
+
 #include <stdio.h>
 #include <conio.h>
-#include<time.h>
+#include <time.h>
 
-char square[10] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+char square[10] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 
 int checkwin();
 void board();
@@ -12,46 +14,45 @@ int main()
     int player = 1, i, choice;
 
     char mark;
-	
-    
+
     do
     {
-    	
+
         board();
         player = (player % 2) ? 1 : 2;
 
         printf("\n\t\t\t   Player %d, enter a number  \n\n\t\t\t\t\t", player);
-       scanf("%d", &choice);
-       mark = (player == 1) ? 'X' : 'O';
-        
+        scanf("%d", &choice);
+        mark = (player == 1) ? 'X' : 'O';
+
         if (choice == 1 && square[1] == '1')
-         square[1] = mark;
+            square[1] = mark;
         else if (choice == 2 && square[2] == '2')
             square[2] = mark;
-            
+
         else if (choice == 3 && square[3] == '3')
             square[3] = mark;
-            
+
         else if (choice == 4 && square[4] == '4')
             square[4] = mark;
-            
+
         else if (choice == 5 && square[5] == '5')
             square[5] = mark;
-         
+
         else if (choice == 6 && square[6] == '6')
             square[6] = mark;
-            
+
         else if (choice == 7 && square[7] == '7')
             square[7] = mark;
-            
+
         else if (choice == 8 && square[8] == '8')
             square[8] = mark;
-            
+
         else if (choice == 9 && square[9] == '9')
-   square[9] = mark;
+            square[9] = mark;
         else
         {
-		
+
             printf("Invalid move ");
 
             player--;
@@ -60,14 +61,14 @@ int main()
         i = checkwin();
 
         player++;
-    }while (i ==  - 1);
-    
+    } while (i == -1);
+
     board();
-     system("color 2f");
+    system("color 2f");
     if (i == 1)
     {
-   printf("\n\n\a\a\t\t\t\t[  Player %d win  ] ", --player);
-}
+        printf("\n\n\a\a\t\t\t\t[  Player %d win  ] ", --player);
+    }
     else
         printf("\n\n\t\t\t\t\a\aGame draw");
 
@@ -80,35 +81,34 @@ int checkwin()
 {
     if (square[1] == square[2] && square[2] == square[3])
         return 1;
-        
+
     else if (square[4] == square[5] && square[5] == square[6])
         return 1;
-        
+
     else if (square[7] == square[8] && square[8] == square[9])
         return 1;
-        
+
     else if (square[1] == square[4] && square[4] == square[7])
         return 1;
-        
+
     else if (square[2] == square[5] && square[5] == square[8])
         return 1;
-        
+
     else if (square[3] == square[6] && square[6] == square[9])
         return 1;
-        
+
     else if (square[1] == square[5] && square[5] == square[9])
         return 1;
-        
+
     else if (square[3] == square[5] && square[5] == square[7])
         return 1;
-        
+
     else if (square[1] != '1' && square[2] != '2' && square[3] != '3' &&
-        square[4] != '4' && square[5] != '5' && square[6] != '6' && square[7] 
-        != '7' && square[8] != '8' && square[9] != '9')
+             square[4] != '4' && square[5] != '5' && square[6] != '6' && square[7] != '7' && square[8] != '8' && square[9] != '9')
 
         return 0;
     else
-        return  - 1;
+        return -1;
 }
 void board()
 {
